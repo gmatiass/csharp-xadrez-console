77a8d5fa21665d991dd2b1cced274b34922cd09d
Gabriel Matias <gmatias1793@gmail.com>
2020-02-21 23:55:21 -0300

    Inserindo alguns tratamentos de erros personalizados.

--- Program.cs
+++ Program.cs
@@ -8,15 +8,21 @@ namespace Projeto_xadrezConsole
     {
         static void Main(string[] args)
         {
-            Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+            try{
+                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
-            tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 0));
-            tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(1, 3));
-            tabuleiro.ColocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(2, 4));
+                tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 0));
+                tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(1, 9));
+                tabuleiro.ColocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(0, 0));
 
-            Tela.ImprimirTabuleiro(tabuleiro);
+                Tela.ImprimirTabuleiro(tabuleiro);
 
-            Console.ReadLine();
+                Console.ReadLine();
+            }
+            catch(TabuleiroException exception){
+                System.Console.WriteLine(exception.Message);
+            }
+            
         }
     }
 }
